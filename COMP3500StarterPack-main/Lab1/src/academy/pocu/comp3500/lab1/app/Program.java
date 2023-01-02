e02f7dce8402a32fc881ca4bfaab3e87f17b3de7
aa <j082175@naver.com>
2023-01-02 10:31:20 +0900

    first

--- COMP3500StarterPack-main/Lab1/src/academy/pocu/comp3500/lab1/app/Program.java
+++ COMP3500StarterPack-main/Lab1/src/academy/pocu/comp3500/lab1/app/Program.java
@@ -1,8 +1,11 @@
 package academy.pocu.comp3500.lab1.app;
 
+import academy.pocu.comp3500.lab1.Lab1;
+
 public class Program {
 
     public static void main(String[] args) {
         // Write your code here
+        System.out.println(Lab1.add(1,2));
     }
 }
