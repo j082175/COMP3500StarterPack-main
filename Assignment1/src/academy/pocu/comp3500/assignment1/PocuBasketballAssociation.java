31ef1d2c4e65c0550ec7cae1d0f72e479e5778ce
aa <j082175@naver.com>
2023-01-03 10:16:56 +0900

    assignment1_44

--- Assignment1/src/academy/pocu/comp3500/assignment1/PocuBasketballAssociation.java
+++ Assignment1/src/academy/pocu/comp3500/assignment1/PocuBasketballAssociation.java
@@ -4,6 +4,55 @@ import academy.pocu.comp3500.assignment1.pba.Player;
 import academy.pocu.comp3500.assignment1.pba.GameStat;
 
 public final class PocuBasketballAssociation {
+    private static int teamwork = 0;
+    private static int playerCount = 0;
+
+    private static void Combination(Player[] players, Player[] scratch, int r, int index, int depth,
+            Player[] outPlayers, int length) {
+
+        if (r == 0) {
+            int min = scratch[0].getAssistsPerGame();
+            int sum = 0;
+            for (int i = 0; i < scratch.length; i++) {
+
+                if (scratch[i] == null) {
+                    break;
+                }
+
+                System.out.print(scratch[i].getName());
+                System.out.print(" ");
+                if (min > scratch[i].getAssistsPerGame()) {
+                    min = scratch[i].getAssistsPerGame();
+                }
+                sum += scratch[i].getPassesPerGame();
+            }
+
+            if (teamwork < sum * min) {
+                teamwork = sum * min;
+                playerCount = length;
+
+                if (outPlayers != null) {
+                    for (int i = 0; i < length; i++) {
+                        outPlayers[i] = scratch[i];
+                    }
+                }
+
+            }
+
+            System.out.println();
+        } else if (depth == players.length) // depth == n // 계속 안뽑다가 r 개를 채우지 못한 경우는 이 곳에 걸려야 한다.
+        {
+            return;
+        } else {
+            // arr[depth] 를 뽑은 경우
+            scratch[index] = players[depth];
+            Combination(players, scratch, r - 1, index + 1, depth + 1, outPlayers, length);
+
+            // arr[depth] 를 뽑지 않은 경우
+            Combination(players, scratch, r, index, depth + 1, outPlayers, length);
+        }
+    }
+
     private PocuBasketballAssociation() {
     }
 
@@ -73,36 +122,81 @@ public final class PocuBasketballAssociation {
         // players[0].getPointsPerGame();
         Player p1 = null;
 
-        int min = players[0].getPointsPerGame() - targetPoints;
-        int minAbs = Math.abs(players[0].getPointsPerGame() - targetPoints);
-        p1 = players[0];
-        for (int i = 1; i < players.length; i++) {
-            if (minAbs > Math.abs(players[i].getPointsPerGame() - targetPoints)) {
-                minAbs = Math.abs(players[i].getPointsPerGame() - targetPoints);
-                p1 = players[i];
+        // int min = players[0].getPointsPerGame() - targetPoints;
+        // int minAbs = Math.abs(players[0].getPointsPerGame() - targetPoints);
+        // p1 = players[0];
+        // for (int i = 1; i < players.length; i++) {
+        // if (minAbs > Math.abs(players[i].getPointsPerGame() - targetPoints)) {
+        // minAbs = Math.abs(players[i].getPointsPerGame() - targetPoints);
+        // p1 = players[i];
+        // }
+
+        // if (minAbs == Math.abs(players[i].getPointsPerGame() - targetPoints)) {
+        // p1 = players[i];
+        // }
+        // }
+
+        int half = players.length / 2;
+        int minAbs = Math.abs(players[half].getPointsPerGame() - targetPoints);
+        p1 = players[half];
+        int count = players.length;
+        while (count != 0) {
+            if (players[half].getPointsPerGame() < targetPoints) {
+                half = players.length - half;
             }
 
-            if (minAbs == Math.abs(players[i].getPointsPerGame() - targetPoints)) {
-                p1 = players[i];
+            if (minAbs > Math.abs(players[half].getPointsPerGame() - targetPoints)) {
+                minAbs = Math.abs(players[half].getPointsPerGame() - targetPoints);
+                p1 = players[half];
             }
+
+            if (minAbs == Math.abs(players[half].getPointsPerGame() - targetPoints)) {
+                p1 = players[half];
+            }
+
+            count /= 2;
         }
 
         return p1;
     }
 
     public static Player findPlayerShootingPercentage(final Player[] players, int targetShootingPercentage) {
+        // players[0].getShootingPercentage();
         return null;
     }
 
     public static long find3ManDreamTeam(final Player[] players, final Player[] outPlayers, final Player[] scratch) {
-        return -1;
+        // players 의 길이는 항상 3보다 같거나 크다고 가정
+        // outPlayers 와 scratch 의 크기는 항상 3이라고 가정
+        // 총 35가지
+
+        Combination(players, scratch, 3, 0, 0, outPlayers, 3);
+
+        int result = teamwork;
+        teamwork = 0;
+
+        return result;
     }
 
     public static long findDreamTeam(final Player[] players, int k, final Player[] outPlayers, final Player[] scratch) {
-        return -1;
+        Combination(players, scratch, k, 0, 0, outPlayers, k);
+
+        int result = teamwork;
+        teamwork = 0;
+
+        return result;
     }
 
     public static int findDreamTeamSize(final Player[] players, final Player[] scratch) {
-        return -1;
+
+        for (int i = 1; i < players.length; i++) {
+            Combination(players, scratch, i, 0, 0, null, i);
+        }
+
+        int result = playerCount;
+        playerCount = 0;
+        teamwork = 0;
+
+        return result;
     }
 }
--- Assignment1/src/academy/pocu/comp3500/assignment1/app/Program.java
+++ Assignment1/src/academy/pocu/comp3500/assignment1/app/Program.java
@@ -7,20 +7,41 @@ import academy.pocu.comp3500.assignment1.pba.Player;
 public class Program {
 
     public static void main(String[] args) {
+        // Player[] players = new Player[] {
+        // new Player("Player 1", 1, 5, 1, 60),
+        // new Player("Player 2", 5, 2, 11, 31),
+        // new Player("Player 3", 7, 4, 7, 44),
+        // new Player("Player 4", 10, 10, 15, 25),
+        // new Player("Player 5", 11, 12, 6, 77),
+        // new Player("Player 6", 15, 0, 12, 61),
+        // new Player("Player 7", 16, 8, 2, 70)
+        // };
+
+        // Player player = PocuBasketballAssociation.findPlayerPointsPerGame(players,
+        // 12); // player: Player 5
+
+        // player = PocuBasketballAssociation.findPlayerPointsPerGame(players, 5); //
+        // player: Player 2
+        // player = PocuBasketballAssociation.findPlayerPointsPerGame(players, 13); //
+        // player: Player 6
+
         Player[] players = new Player[] {
-                new Player("Player 1", 1, 5, 1, 60),
-                new Player("Player 2", 5, 2, 11, 31),
-                new Player("Player 3", 7, 4, 7, 44),
-                new Player("Player 4", 10, 10, 15, 25),
-                new Player("Player 5", 11, 12, 6, 77),
-                new Player("Player 6", 15, 0, 12, 61),
-                new Player("Player 7", 16, 8, 2, 70)
+                new Player("Player 1", 2, 5, 10, 78),
+                new Player("Player 2", 10, 4, 5, 66),
+                new Player("Player 3", 3, 3, 2, 22),
+                new Player("Player 4", 1, 9, 8, 12),
+                new Player("Player 5", 11, 1, 12, 26),
+                new Player("Player 6", 7, 2, 10, 15),
+                new Player("Player 7", 8, 15, 3, 11),
+                new Player("Player 8", 5, 7, 13, 5),
+                new Player("Player 9", 8, 2, 7, 67),
+                new Player("Player 10", 1, 11, 1, 29),
+                new Player("Player 11", 2, 6, 9, 88)
         };
 
-        Player player = PocuBasketballAssociation.findPlayerPointsPerGame(players, 12); // player: Player 5
+        Player[] scratch = new Player[players.length];
 
-        player = PocuBasketballAssociation.findPlayerPointsPerGame(players, 5); // player: Player 2
-        player = PocuBasketballAssociation.findPlayerPointsPerGame(players, 13); // player: Player 6
+        int k = PocuBasketballAssociation.findDreamTeamSize(players, scratch); // k: 6
 
         int a = 1;
     }
